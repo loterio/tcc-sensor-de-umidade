9c814416f8329e267813074c0d304192f70c9780
fabio loterio <vitorloterio@gmail.com>
2020-09-01 11:54:20 -0300

    fixed problem in sql file

--- banco-de-dados/SMUS-Estrutura.sql
+++ banco-de-dados/SMUS-Estrutura.sql
@@ -5,36 +5,33 @@ USE SMUS;
 
 CREATE TABLE sensor (
 	id int auto_increment not null,
-	umidade float not null,
+    umidade float not null,
     latitude float not null,
     longitude float not null,
     primary key(id)
 );
 
-CREATE TABLE cliente (
+CREATE TABLE quadroTerra (
 	id int auto_increment not null,
-	login varchar(40) unique not null,
-	email varchar(45) unique not null,
-    senha varchar(30) not null,
+    nome varchar(45) not null,
     primary key(id)
 );
 
-CREATE TABLE quadroTerra (
-	id int auto_increment not null,
-    nome varchar(45) unique not null,
-    idCliente varchar(40) not null,
-    primary key(id),
-    foreign key(idCliente) references cliente(id)
-);
-
 CREATE TABLE leitura (
-	dataHora datetime not null,
+	dataHora timestamp not null,
     idSensor int not null,
     idQuadroTerra int not null,
     umidade float not null,
     primary key(dataHora, idSensor, idQuadroTerra),
     foreign key(idSensor) references sensor(id),
     foreign key(idQuadroTerra) references quadroTerra(id)
+);
+
+CREATE TABLE cliente (
+	login varchar(40) not null,
+	email varchar(45) not null,
+    senha varchar(30) not null,
+    primary key(login)
 );
 
 INSERT INTO sensor(umidade, latitude, longitude) VALUES
@@ -44,21 +41,21 @@ INSERT INTO sensor(umidade, latitude, longitude) VALUES
 (27.5,84.270,-89.844),
 (26.5,90.010,-90.568);
 
-INSERT INTO quadroTerra(nome, idCliente) VALUES 
-("Roça de baixo", 1),
-("Divisa terreno", 1),
-("Perto estrada geral", 1);
+INSERT INTO quadroTerra(nome) VALUES 
+("roça de baixo"),
+("divisa terreno"),
+("perto estrada geral");
 
 INSERT INTO cliente(login,email,senha) VALUES 
 ('Henrique', 'Henrique@gmail.com', '123'); 
 
 INSERT INTO leitura(dataHora,idSensor,idQuadroTerra,umidade) VALUES 
-('2020-09-02 12:05:36.0000000',1,1,31.50),
-('2020-09-02 12:00:36.0000000',1,1,30.50),
-('2020-09-02 12:05:36.0000000',2,1,32.20),
-('2020-09-02 12:10:36.0000000',5,1,25.40),
-('2020-09-02 12:15:36.0000000',3,1,22.30),
-('2020-09-02 12:15:36.0000000',4,1,28.7),
-('2020-09-02 12:15:36.0000000',2,2,47.00),
-('2020-09-02 12:10:36.0000000',1,2,42.80),
-('2020-09-02 12:00:36.0000000',5,2,62.1);
+('2020-09-02 12:05:36',1,1,31.50),
+('2020-09-02 12:00:36',1,1,30.50),
+('2020-09-02 12:05:36',2,1,32.20),
+('2020-09-02 12:10:36',5,1,25.40),
+('2020-09-02 12:15:36',3,1,22.30),
+('2020-09-02 12:15:36',4,1,28.7),
+('2020-09-02 12:15:36',2,2,47.00),
+('2020-09-02 12:10:36',1,2,42.80),
+('2020-09-02 12:00:36',5,2,62.1);
