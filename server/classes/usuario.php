9354e9d05359d163aa6ec42d7f12dd609e4b920d
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2020-09-14 11:08:06 -0300

    usuario zerado

--- server/classes/usuario.php
+++ server/classes/usuario.php
@@ -2,10 +2,5 @@
   require_once('../Conexao.php');
   $pdo = new PDO(MYSQL, USER, PASSWORD);
 
-  $res = $pdo->prepare("SELECT *
-                              FROM cliente");
-  $res->execute();
-  $resultado = $res->fetch(PDO::FETCH_ASSOC);
-  $resultado = json_encode($resultado);
-  echo $resultado;
+  
 ?>
