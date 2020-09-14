980fd4d3d6d6dcaf7b8decea96e1c40fe3a3eb98
fabio loterio <vitorloterio@gmail.com>
2020-09-14 13:16:04 -0300

    user showing json

--- server/classes/usuario.php
+++ server/classes/usuario.php
@@ -1,6 +1,18 @@
 <?php
-  require_once('../Conexao.php');
-  $pdo = new PDO(MYSQL, USER, PASSWORD);
 
-  
+  require_once('../Conexao.php');
+
+  try{
+    
+    $pdo = new PDO(MYSQL, USER, PASSWORD);
+    $res = $pdo->prepare("SELECT * FROM cliente;");
+    $res->execute();
+    $resultado = $res->fetch(PDO::FETCH_ASSOC);
+    $resultado = json_encode($resultado);
+    echo $resultado;
+
+	} catch(PDOException $e) {
+		echo '<strong>Error:</strong> '.$e->getMessage();
+  }  
+
 ?>
