5daad7313a3de6f07534256aba398707132b2498
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2020-09-15 11:21:38 -0300

    Alteração do da estrutura banco de dados

--- DB/SMUS-Estrutura.sql
+++ DB/SMUS-Estrutura.sql
@@ -3,30 +3,6 @@ DROP DATABASE SMUS;
 CREATE DATABASE SMUS; 
 USE SMUS;
 
-CREATE TABLE IF NOT EXISTS sensor (
-	id int auto_increment not null,
-    umidade float not null,
-    latitude float not null,
-    longitude float not null,
-    primary key(id)
-);
-
-CREATE TABLE IF NOT EXISTS quadroTerra (
-	id int auto_increment not null,
-    nome varchar(45) not null,
-    primary key(id)
-);
-
-CREATE TABLE IF NOT EXISTS leitura (
-	dataHora timestamp not null,
-    idSensor int not null,
-    idQuadroTerra int not null,
-    umidade float not null,
-    primary key(dataHora, idSensor, idQuadroTerra),
-    foreign key(idSensor) references sensor(id),
-    foreign key(idQuadroTerra) references quadroTerra(id)
-);
-
 CREATE TABLE IF NOT EXISTS usuario (
 	id int auto_increment not null,
 	login varchar(40) not null unique,
@@ -35,30 +11,56 @@ CREATE TABLE IF NOT EXISTS usuario (
     primary key(id)
 );
 
-INSERT INTO sensor(umidade, latitude, longitude) VALUES
-(24.5,78.542,-99.274),
-(25.5,80.712,-85.116),
-(23.5,87.854,-94.432),
-(27.5,84.270,-89.844),
-(26.5,90.010,-90.568);
+CREATE TABLE IF NOT EXISTS quadroTerra (
+	id int auto_increment not null,
+    nome varchar(45) not null,
+    proprietario int not null,
+    foreign key(proprietario) references usuario(id),
+    primary key(id)
+);
 
-INSERT INTO quadroTerra(nome) VALUES 
-("roça de baixo"),
-("divisa terreno"),
-("perto estrada geral");
+CREATE TABLE IF NOT EXISTS sensor (
+	id int auto_increment not null,
+    idQuadroTerra int,
+    latitude float not null,
+    longitude float not null,
+    foreign key(idQuadroTerra) references quadroTerra(id),
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS leitura (
+	dataHora timestamp not null,
+    idSensor int not null,
+    umidade float not null,
+    primary key(dataHora, idSensor),
+    foreign key(idSensor) references sensor(id)
+);
 
 INSERT INTO usuario(login,email,senha) VALUES 
 ('Henrique', 'Henrique@gmail.com', '123'),
 ('fabio','vitor@gmail.com','ashw'),
 ('nelso','nl@hotmail.com','abc123'); 
 
-INSERT INTO leitura(dataHora,idSensor,idQuadroTerra,umidade) VALUES 
-('2020-09-02 12:05:36',1,1,31.50),
-('2020-09-02 12:00:36',1,1,30.50),
-('2020-09-02 12:05:36',2,1,32.20),
-('2020-09-02 12:10:36',5,1,25.40),
-('2020-09-02 12:15:36',3,1,22.30),
-('2020-09-02 12:15:36',4,1,28.7),
-('2020-09-02 12:15:36',2,2,47.00),
-('2020-09-02 12:10:36',1,2,42.80),
-('2020-09-02 12:00:36',5,2,62.1);
+INSERT INTO quadroTerra(proprietario, nome) VALUES 
+(1,"roça de baixo"),
+(1,"divisa terreno"),
+(1,"perto estrada geral");
+
+INSERT INTO sensor(idQuadroTerra, latitude, longitude) VALUES
+(1,78.542,-99.274),
+(1,80.712,-85.116),
+(2,87.854,-94.432),
+(2,84.270,-89.844),
+(3,90.010,-90.568);
+
+
+INSERT INTO leitura(dataHora,idSensor,umidade) VALUES 
+('2020-09-02 12:05:36',1,31.50),
+('2020-09-02 12:00:36',1,30.50),
+('2020-09-02 12:05:36',2,32.20),
+('2020-09-02 12:10:36',5,25.40),
+('2020-09-02 12:15:36',3,22.30),
+('2020-09-02 12:15:36',4,28.7),
+('2020-09-02 12:15:36',2,47.00),
+('2020-09-02 12:10:36',1,42.80),
+('2020-09-02 12:00:36',5,62.1);
