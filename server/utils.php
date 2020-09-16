320ce4e1845951ba8975275cd895ee72990bfa03
Henrique Borges dos Santos <henrique.10agr@gmail.com>
2020-09-16 11:31:30 -0300

    Utils - D(Documento onde serão criadas as funções auxiliares)

---
+++ server/utils.php
@@ -0,0 +1,24 @@
+<?php
+  require_once('Conexao.php');
+
+  function executaComandoSql($comando, $arrayStrings, $arrayValores) {
+    $pdo             = new PDO(MYSQL, USER, PASSWORD);
+    $busca           = $pdo->prepare($comando);
+    $numeroDeValores = count($arrayStrings);
+
+    for ($i=0; $i < $numeroDeValores; $i++) { 
+      $busca->bindparam($arrayStrings[$i], $arrayValores[$i]);
+    }
+
+    $busca->execute();
+    return $busca;
+  }
+
+  function retornaJsonSelect($busca) {
+    $resultado = array();
+      while($linha = $busca->fetch(PDO::FETCH_ASSOC)){
+        array_push($resultado, $linha); 
+      }
+      $resultado = json_encode($resultado);
+      return $resultado;
+  }
