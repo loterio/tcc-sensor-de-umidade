5767ed1cbb3d6fac1375d7af77bf008555f90bb3
fabio loterio <vitorloterio@gmail.com>
2020-09-17 10:10:12 -0300

    a

--- DB/SMUS-Comandos.sql
+++ DB/SMUS-Comandos.sql
@@ -1,5 +1,9 @@
 USE SMUS;
 
+INSERT INTO quadroTerra(proprietario, nome) 
+                  VALUES (3,'paiol');
+
+select * from quadroTerra;
 SELECT id as "# Quadro", nome, proprietario, idSensor, latitude, longitude 
 FROM quadroTerra
 	INNER JOIN sensor
@@ -10,4 +14,4 @@ UPDATE usuario
 SET login = 'Fabio Loterio',
 	email = 'vitorloterio@gmail.com',
 	senha = 'schneider'
- WHERE login LIKE 'fabio';
+ WHERE login LIKE ;
